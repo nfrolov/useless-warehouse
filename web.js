ab79306f79ebae4f029c033d0fcbf5a0b494b553
Nikita Frolov <nikita@frolov.fi>
2014-05-18 22:54:47 +0300

    use built-in static middleware

--- web.js
+++ web.js
@@ -1,9 +1,8 @@
 var express = require('express'),
     morgan = require('morgan'),
-    static = require('serve-static'),
     app = express();
 
 app.use(morgan('short'));
-app.use(static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 5000);
