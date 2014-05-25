84d32b9ad12812a77f1fe2a377a926a2756b9aef
Nikita Frolov <nikita@frolov.fi>
2014-05-25 19:11:05 +0300

    use strict routing

--- web.js
+++ web.js
@@ -2,6 +2,9 @@ var express = require('express'),
     morgan = require('morgan'),
     app = express();
 
+app.enable('case sensitive routing');
+app.enable('strict routing');
+
 app.use(morgan('short'));
 app.use(express.static(__dirname + '/public'));
 
