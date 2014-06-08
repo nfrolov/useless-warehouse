4981d15fba99c913d5862fdd9206eb4b716aa2f9
Nikita Frolov <nikita@frolov.fi>
2014-06-08 22:49:56 +0300

    test data modifications

--- sql/add-test-data.sql
+++ sql/add-test-data.sql
@@ -1,3 +1,7 @@
+truncate table warehouse.account restart identity cascade;
+truncate table warehouse.category restart identity cascade;
+
+
 -- password is "password"
 insert into warehouse.account (account_id, username, password)
   values (1, 'worker', '$2a$16$ugm5RM4oae8qTKEdAyNOkOOzN2I6D6kxGULVx2fcZg3xEQOzdYRvK');
@@ -12,3 +16,7 @@ insert into warehouse.client (client_id, name, credit_limit)
 
 insert into warehouse.category (category_id, name)
   values (1, 'Teleport'), (2, 'Perpetual motion');
+
+
+alter sequence warehouse.account_account_id_seq restart with 3;
+alter sequence warehouse.category_category_id_seq restart with 3;
